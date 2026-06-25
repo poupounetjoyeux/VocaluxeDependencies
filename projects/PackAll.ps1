dd8a39b8376d0537f3825e9c87235aaee9c7207d
brayquentin <brayquentin@gmail.com>
2026-06-25 15:24:44 +0200

    Remove FFmpeg v4.1.2 & Acinerella
    Make some DLLs inclusion more restrictive and add missing PDBs
    Fix PackAll.ps1 to pack vocalux.dependencies at the end

--- projects/PackAll.ps1
+++ projects/PackAll.ps1
@@ -1,5 +1,14 @@
+$buildDependenciesProject = "vocaluxe.dependencies.csproj"
 $dependencyProjects = Get-ChildItem -Path "." -Filter "*.csproj" -Recurse -File
 foreach ($dependencyProject in $dependencyProjects) {
+	if($($dependencyProject.Name) -eq $buildDependenciesProject) {
+		continue
+	}
+	
 	Write-Output "Packing $($dependencyProject.Name)"
 	dotnet pack $($dependencyProject.FullName)
 }
+
+$buildDependenciesProjectPath = Join-Path "dependencies" $buildDependenciesProject
+Write-Output "Now packing build dependencies"
+dotnet pack $buildDependenciesProjectPath
